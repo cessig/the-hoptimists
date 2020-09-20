d62a480a6203ca8943bd14bab13340a9a7b81b76
cessig <cessig4161994@gmail.com>
2020-09-20 11:32:31 -0400

    index route made

--- controllers/beer.js
+++ controllers/beer.js
@@ -5,6 +5,16 @@ const db = require("../models");
 // base route is /beers
 
 // index
+router.get("/", function (req, res) {
+  db.Beer.find({}, function (error, foundBeers) {
+    if (error) return res.send(error);
+
+    const context = {
+      beers: foundBeers,
+    };
+    res.render("beer/index", context);
+  });
+});
 
 // new
 
--- models/index.js
+++ models/index.js
@@ -21,4 +21,6 @@ mongoose.connection.on("disconnect", function (event) {
   console.log("Mongodb disconnected", event);
 });
 
-module.exports = {};
+module.exports = {
+  Beer: require("./Beer"),
+};
