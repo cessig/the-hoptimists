efc90f88d9d940fa4074e1d203da2a394bd2e855
cessig <cessig4161994@gmail.com>
2020-09-20 11:44:51 -0400

    New beer route created

--- controllers/beer.js
+++ controllers/beer.js
@@ -17,6 +17,17 @@ router.get("/", function (req, res) {
 });
 
 // new
+router.get("/new", function (req, res) {
+  db.Beer.find({}, function (error, foundBeers) {
+    if (error) return res.send(error);
+
+    const context = {
+      beers: foundBeers,
+    };
+
+    res.render("beer/new", context);
+  });
+});
 
 // create
 
