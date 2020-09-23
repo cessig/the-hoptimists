adb701bed8bd19aba64f02f31b849a929a2efdad
cessig <cessig4161994@gmail.com>
2020-09-23 12:52:45 -0400

    Add the required middleware in server js for authorazation as well as added the authRequired function to our beer and brewery routes

--- server.js
+++ server.js
@@ -21,6 +21,27 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: null,
+    store: new MongoStore({
+      url: "mongodb://localhost:27017/the-hoptimists",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 24 * 7 * 2,
+    },
+  })
+);
+
+const authRequried = function (req, res, next) {
+  if (!req.session.currentUser) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
 /* Routes */
 
 // Views routes
@@ -30,6 +51,7 @@ app.get("/", function (req, res) {
 });
 
 // Auth Routes
+app.use("/", authRequired, controllers.auth);
 
 // Beer Routes
 app.use("/beers", controllers.beer);
